d23c8cf1ee783b1d0d911943791f51536a2a9f24
Script-Savant <alexkienjeku@gmail.com>
2025-05-26 15:19:52 +0300

    Getting started

--- GetStartedApp/GetStartedApp/obj/Debug/net9.0/GetStartedApp.AssemblyInfo.cs
+++ GetStartedApp/GetStartedApp/obj/Debug/net9.0/GetStartedApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5cdfb81178f0b8de3c73a1f1a1ac7a28b6d04c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
