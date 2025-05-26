cf71ffa5320caafb8a01a6ae49f2a798b5ee2eff
Script-Savant <alexkienjeku@gmail.com>
2025-05-26 22:51:22 +0300

    init

---
+++ PersonCrud/PersonCrud/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PersonCrud.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ PersonCrud/PersonCrud/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PersonCrud.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
