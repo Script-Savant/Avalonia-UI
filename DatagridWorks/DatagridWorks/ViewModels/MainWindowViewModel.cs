efd3bff60ff3928a83b64ce74cc111ed80a9fdf2
Script-Savant <alexkienjeku@gmail.com>
2025-05-31 20:25:16 +0300

    add data

--- DatagridWorks/DatagridWorks/ViewModels/MainWindowViewModel.cs
+++ DatagridWorks/DatagridWorks/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace DatagridWorks.ViewModels;
+﻿using System.Collections.ObjectModel;
+using DatagridWorks.Models;
+using System.Collections.Generic;
+
+namespace DatagridWorks.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public ObservableCollection<Person> People { get; }
+
+    public MainWindowViewModel()
+    {
+        var people = new List<Person>
+        {
+            new Person("Neil", "Armstrong"),
+            new Person("Jane", "Smith"),
+            new Person("John", "Doe")
+        };
+
+        People = new ObservableCollection<Person>(people);
+    }
 }
